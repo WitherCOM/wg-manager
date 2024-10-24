14cf6d030f02e4b3e8d8466ebe706ca87631b0c2
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2024-10-24 13:00:33 +0200

    Update entrypoint

--- wg/overlay/entrypoint.sh
+++ wg/overlay/entrypoint.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
-# get servers
-response=$(curl "$APP_URL/api/servers" -H "Api-Key: $TOKEN" -H "Content-Type: application/json" | jq -r '.data[] | "\(.name) \(.id)"')
+setup_wireguard() {
+  # disable interfaces
+  wg-quick down *
 
-# create servers
-echo "$response" | while read -r line; do
-# Split the line into name and id
-name=$(echo "$line" | awk '{print $1}')
-id=$(echo "$line" | awk '{print $2}')
-echo "Adding $name"
-curl "$APP_URL/api/server/$id" -H "Api-Key: $TOKEN" -H "Content-Type: application/json" > "/etc/wireguard/$name.conf"
-wg-quick up $name
-done
+  # clear configs
+  rm /etc/wireguard/*.conf 
 
-#infinite loop
+  # get servers
+  response=$(curl "$APP_URL/api/servers" -H "Api-Key: $TOKEN" -H "Content-Type: application/json" | jq -r '.data[] | "\(.name) \(.id)"')
+
+  # create servers
+  echo "$response" | while read -r line; do
+  # Split the line into name and id
+  name=$(echo "$line" | awk '{print $1}')
+  id=$(echo "$line" | awk '{print $2}')
+  echo "Adding $name"
+  curl "$APP_URL/api/server/$id" -H "Api-Key: $TOKEN" -H "Content-Type: application/json" > "/etc/wireguard/$name.conf"
+  wg-quick up $name
+  done
+}
+
+#infinite loop, refresh after 20 minutes
 while true
 do
-  sleep 60
+  setup_wireguard()
+  sleep 1200
 done
